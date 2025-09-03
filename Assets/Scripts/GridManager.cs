686a4ba791138b63951bb503997b9b316c59ed71
Kai Yasko <yasko.kai@gmail.com>
2025-09-03 13:03:30 -0400

    more movement

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -10,4 +10,13 @@ public class GridManager : MonoBehaviour
 
     }
 
+	public bool ZoneInBounds(Vector2Int pos, GridManager gridManager)
+	{
+		return (
+			pos.x > -1 &&
+			pos.y > -1 &&
+			pos.x < gridManager.cols &&
+			pos.y < gridManager.rows 
+		);
+	}
 }
